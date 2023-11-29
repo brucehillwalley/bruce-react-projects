64e6a250b50f7db0479b5f91e1c99c76f76d195c
BruceHill <brucehillwalley@gmail.com>
2023-11-30 00:57:46 +0300

    first commit

---
+++ person-list-project/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const App =()=>(<PersonList/>)
+
+
+const PersonList=()=>{
+  return(
+    <Person/>
+  )
+}
+
+const Person=()=>{
+  return(
+    <div>i'm person component</div>
+  )
+}
+
+
+
+
+export default App;
