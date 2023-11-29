8417102206614ed7726e82e33e2302412c5e4cb9
BruceHill <brucehillwalley@gmail.com>
2023-11-30 02:10:53 +0300

    fineshed

--- person-list-project/src/App.js
+++ person-list-project/src/App.js
@@ -1,21 +1,46 @@
-import './App.css';
+import "./App.css";
 
-const App =()=>(<PersonList/>)
+const App = () => <PersonList />;
 
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "john",
+      jop: "developer",
+    },
+    { img: 34, name: "Bob", jop: "artist" },
+    { img: 56, name: "Peter", jop: "pilot" },
+  ];
 
-const PersonList=()=>{
-  return(
-    <Person/>
-  )
-}
+  return (
+    <section>
+      <Person person={people[0]}/>
+      <Person person={people[1]}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum delectus culpa ullam quidem tenetur, veritatis
+        {/* burayı children property si tutuyor */}
+      </Person>
+      <Person person={people[2]}/>
+    </section>
+  );
+};
 
-const Person=()=>{
-  return(
-    <div>i'm person component</div>
-  )
-}
+const Person = (props) => {
+  const {img,name,jop}=props.person
+  const {children}=props
+  
+  const url = `https://randomuser.me/api/portraits/med/men/${img}.jpg`;
 
-
-
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{jop}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default App;
